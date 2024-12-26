5cc9b421c48700fd7cea0a68f5d830cf997cbf4d
721150 <144417455+721150@users.noreply.github.com>
2024-12-26 14:41:38 +0100

    v43
    Aanpassen inloggen

--- app/controllers/homecontroller.php
+++ app/controllers/homecontroller.php
@@ -16,7 +16,15 @@ class HomeController {
         if (!$loginController->checkLogin()) {
             $loginController->login();
         } else {
-            require __DIR__ . '/../views/dashboard/studentdasboard.php';
+            if (isset($_SESSION['student'])) {
+                require __DIR__ . '/../views/dashboard/studentdasboard.php';
+            }
+            else if (isset($_SESSION['teacher'])) {
+                http_response_code(404);
+            }
+            else if (isset($_SESSION['admin'])) {
+                http_response_code(404);
+            }
             exit;
         }
     }
